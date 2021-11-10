3deaddcbbfd820eb99a8434f48e9e760dafeabc3
Saymon Victor <sayminnv88@gmail.com>
2021-11-10 10:20:27 -0300

    add functions play, pause and stop

--- scrip.js
+++ scrip.js
@@ -1,19 +1,24 @@
-let music = document.getElementById('audio-music')
+let music = document.querySelector('.music')
 let cont = 0
 
 function play() {
-  if (cont % 2 === 0) {
-    music.play()
-    document.getElementById('play').src = './assets/img/pause_circle.svg'
-  } else {
-    document.getElementById('play').src = './assets/img/play_circle.svg'
-  }
-  cont++
+  music.play()
+  document.querySelector('.control-play').style.display = 'none'
+  document.querySelector('.control-pause').style.display = 'block'
+  document.querySelector('.control-stop').style.display = 'block'
 }
+function pause() {
+  music.pause()
+  document.querySelector('.control-play').style.display = 'block'
+  document.querySelector('.control-pause').style.display = 'none'
+}
+
 function stop() {
   music.pause()
   music.currentTime = 0
-  
+  document.querySelector('.control-play').style.display = 'block'
+  document.querySelector('.control-pause').style.display = 'none'
+  document.querySelector('.control-stop').style.display = 'none'
 }
 function skip_previous() {}
 function skip_next() {}
